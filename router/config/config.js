4033e7f8731ffdaef617a57b5d874d0f27870863
“your <sudhir_s@tekditechnologies.com>
2020-04-03 15:23:32 +0530

    Issue #001 Set session value in redis

--- router/config/config.js
+++ router/config/config.js
@@ -15,10 +15,12 @@ config.HTTPS_PATH_CERT               = ''
 config.HTTPS_PATH_CA                 = ''
 
 
+config.REDIS_PORT					 = 6379
 config.ELASTIC_HOST                  = 'http://<user>:<password><IP>:<Port>'
 config.ELASTIC_INDEX_NAME            = 'indx_name'
 config.ELASTIC_INDEX_TYPE            = 'indx_type'
 
-config.REDIS_PORT					 = 6379
+config.TELEGRAM_BOT_ENDPOINT         = 'https://api.telegram.org/bot<bot_secret_key>/sendMessage'
+
 
 module.exports = config;
