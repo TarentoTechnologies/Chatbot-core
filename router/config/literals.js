8c56583c932b66f7a6a8cb0a031b957cc71ba5db
Mathew Jose Pallan <mathewjpallan@gmail.com>
2020-04-13 19:58:13 +0530

    Updated literals

--- router/config/literals.js
+++ router/config/literals.js
@@ -1,9 +1,9 @@
 var message                       = {}
 
 message.START = "<span>Hi, I’m TARA, your DIKSHA assistant. How may I help you today? <br> Select from one of the following options <ol> <li> To find and play content </li> <li> To download/update the DIKSHA mobile app </li><li> Other Queries</li></ol></span>"
-message.CHOOSE_BOARD = "<span> Tell me which of the following board you belong to, so that I can assist you better Send the number corresponding to the board.: <ol> <li> 1. CBSE  </li><li> 2. State Board </li> </ol> </span>"
+message.CHOOSE_BOARD = "<span> Tell me which of the following board you belong to, so that I can assist you better Send the number corresponding to the board.: <ol> <li> CBSE</li><li> State Board </li> </ol> </span>"
 message.CBSE_MESSAGE = "<span> Please visit (<a href='https://diksha.gov.in/CBSE/explore'>diksha.gov.in/CBSE/explore</a>) to access eResources, videos, DFs and practice sheets from the topics in your syllabus. <br> Press 9 to go back to the previous menu or 0 to the main menu </span>"
-message.CHOOSE_STATE_BOARD = "<span>Which of the following state boards you belong to? <ol><li> 1. Tamil Nadu </li><li> 2. Karnataka </li><li> 3. Gujarat </li><li> 4. Uttar Pradesh </li> </ol> </span>"
+message.CHOOSE_STATE_BOARD = "<span>Which of the following state boards you belong to? <ol><li> Tamil Nadu </li><li> Karnataka </li><li> Gujarat </li><li> Uttar Pradesh </li> </ol> </span>"
 message.TN_BOARD = "<span>Please visit (<a href='https://diksha.gov.in/tn/explore'>diksha.gov.in/tn/explore</a>) to access eResources, videos, PDFs and practice sheets from the topics in your syllabus. Press 9 to go back to the previous menu or 0 to the main menu.</span>"
 message.KA_BOARD = "<span>Please visit (<a href='https://diksha.gov.in/ka/explore'>diksha.gov.in/ka/explore</a>) to access eResources, videos, PDFs and practice sheets from the topics in your syllabus. Press 9 to go back to the previous menu or 0 to the main menu.</span>"
 message.GJ_BOARD = "<span>Please visit (<a href='https://diksha.gov.in/gj/explore'>diksha.gov.in/gj/explore</a>) to access eResources, videos, PDFs and practice sheets from the topics in your syllabus. Press 9 to go back to the previous menu or 0 to the main menu.</span>"
