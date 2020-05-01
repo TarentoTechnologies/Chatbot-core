d53e5896fd74fbd845399be2f93f9a1abe291bb4
“your <sudhir_s@tekditechnologies.com>
2020-05-01 17:53:33 +0530

    Added comment for why this change is done

--- router/log/logger.js
+++ router/log/logger.js
@@ -3,6 +3,7 @@ var winston = require('winston');
 var logger = winston.createLogger({
   transports: [
     new (winston.transports.Console)({ json: false, timestamp: true }),
+    // Commenting the below line as we will not need errors/info logging into file , We are already loggin errors/info in console.
     // new winston.transports.File({ filename: __dirname + '/debug.log', json: false })
   ],
   exceptionHandlers: [
